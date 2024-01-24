8789b0c3c039ebf1f992bb253068ea865173544a
Никита <55665370+KoverSamolet@users.noreply.github.com>
2024-01-24 05:01:28 +0300

    first commit

---
+++ cmake-build-debug/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/Chayka/Desktop/search_engine
+# Build directory: C:/Users/Chayka/Desktop/search_engine/cmake-build-debug
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("nlohmann_json")
+subdirs("Google_tests/lib")
